23520dd196a83d3f67d2c4922d7b0af9a2018652
wangzhengqi <fly_zqw@sina.com>
2019-03-29 17:55:45 +0800

    first commit

---
+++ CustomLargeNavTitle-master/LargeNavTitleDemo/LargeNavTitleDemo/LargeNavTitle/PushedViewController.h
@@ -0,0 +1,17 @@
+//
+//  PushedViewController.h
+//  LargeNavTitleDemo
+//
+//  Created by AnDong on 2019/3/29.
+//  Copyright © 2019年 tigerAndBull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PushedViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
